6402b9662074de6b449f95e471dfd651feb26281
fellipebastos <fellipebastoswm@gmail.com>
2023-02-08 07:36:32 -0300

    feat: add Google Calendar integration

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -42,6 +42,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (error) {
       if (error instanceof AxiosError && error?.response?.data?.error) {
         alert(error.response.data.error)
